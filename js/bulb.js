ae8309bcbd43b8a2877351f2c60d37913a84d9b9
experionsachin <sachin.kumar@experionglobal.com>
2017-02-07 14:27:22 +0530

    latest

---
+++ js/bulb.js
@@ -0,0 +1,7 @@
+function turnOnLight() {
+	document.getElementById('light').src='images/pic_bulbon.gif';
+}
+
+function turnOffLight() {
+	document.getElementById('light').src='images/pic_bulboff.gif';
+}
